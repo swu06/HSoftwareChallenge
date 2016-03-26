5d3640c68371d6ec5d8ae291606b8e967b73c3e2
swu <sarahjanewu@gmail.com>
2016-03-26 17:15:43 -0500

    Project Completed.

--- HSoftwareChallenge/src/JsonCleaner.java
+++ HSoftwareChallenge/src/JsonCleaner.java
@@ -4,60 +4,159 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
 import javax.json.JsonStructure;
+import javax.json.JsonValue;
 import javax.json.JsonWriter;
-import javax.json.stream.JsonGenerator;
-import javax.json.stream.JsonParser;
-import javax.json.stream.JsonParser.Event;
 
 /*
- * This class removes redundancy in a JSON file by removing items within children elements that have already
- * appeared in the parent. Duplicate items are detected by comparing the name of the object.  This class writes
- * out the resulting file at the same time the input is read and a main method is provided to allow
- * default access.
+ * This class removes redundancy in a JSON file by removing items within 
+ * children elements that have already appeared in the parent. Duplicate items
+ * are detected by comparing the name of the object.  This class writes out the
+ * cleaned Json file to the writer provided.
+ * 
+ * @author Sarah Wu
  */
 public class JsonCleaner {
-	JsonStructure jsonst;
-	
+	JsonStructure oldFile;
+	JsonStructure newFile;
+
+	/*
+	 * Create our cleaner object.
+	 * 
+	 * @param reader a valid reader object that contains the Json File data
+	 */
 	public JsonCleaner(Reader reader) {
 		JsonReader jReader = Json.createReader(reader);
-		jsonst = jReader.read();
-	}
-	
-	public void cleanDuplicates() {
-		
-	}
-	
-	public void write(Writer writer) {
-		JsonWriter jsonWriter = Json.createWriter(writer);
-		jsonWriter.write(jsonst);
+		oldFile = jReader.read();
+		newFile = oldFile;
 	}
 
+	/*
+	 * Cleans the Json Structure that was read in by the constructor
+	 */
+	public void cleanDuplicates() {
+		Set<String> emptyKeys = new HashSet<String>();
+		newFile = (JsonStructure) cleanDuplicatesRecursively(oldFile, emptyKeys);
+	}
+
+	/*
+	 * Actual method that cleans the JsonFile. It is called recursively over the
+	 * whole tree of the Json file
+	 * 
+	 * @param: currentTree the JsonValue that represents the current section of
+	 * the JsonFile
+	 * 
+	 * @param: previousKeys the set of keys that were found within the parent
+	 * sections of the Json File
+	 */
+	private JsonValue cleanDuplicatesRecursively(JsonValue currentTree, 
+												Set<String> previousKeys) {
+		// All values are passed directly through except the two cases below
+		JsonValue toReturn = currentTree;
+		
+		if (currentTree.getValueType() == JsonValue.ValueType.OBJECT) {
+			// JsonObjects contain multiple items and require iterating
+			JsonObject currentObject = (JsonObject) currentTree;
+
+			// Gather objects from this level and add them to our list of keys
+			Set<String> currentKeys = currentObject.keySet();
+			Set<String> combinedKeys = new HashSet<String>(currentKeys);
+			combinedKeys.addAll(previousKeys);
+
+			// Iterate over all child values
+			JsonObjectBuilder currentModel = Json.createObjectBuilder();
+			for (String key : currentKeys) {
+				// Check to make sure this key hasn't been seen before.
+				if (!previousKeys.contains(key)) {
+					currentModel.add(key, 
+							cleanDuplicatesRecursively(currentObject.get(key), combinedKeys));
+				}
+			}
+
+			toReturn = currentModel.build();
+
+		} else if (currentTree.getValueType() == JsonValue.ValueType.ARRAY) {
+			// JsonArrays also contain multiple objects and require iterating
+			JsonArray currentArray = (JsonArray) currentTree;
+
+			// (No additional keys to grab at this level)
+
+			// Iterate over all child values
+			JsonArrayBuilder currentModel = Json.createArrayBuilder();
+			for (JsonValue currentValue : currentArray) {
+				currentModel.add(cleanDuplicatesRecursively(currentValue, previousKeys));
+			}
+
+			toReturn = currentModel.build();
+	
+		}
+		
+		return toReturn;
+
+	}
+
+	/*
+	 * This method writes the "current" JsonStructure to the writer provided.
+	 * Note that the cleanDuplicates method should be called first if the user
+	 * wants to write out the structure without duplicate values. This method
+	 * can be called repeatedly to write out multiple copies of the Json file
+	 * that this class represents.
+	 * 
+	 * @param writer an object that can write out to the correct location
+	 */
+	public void write(Writer writer) {
+		JsonWriter jsonWriter = Json.createWriter(writer);
+		jsonWriter.write(newFile);
+	}
+
+	/*
+	 * This main method is provided so this class can be called by batch
+	 * processes or other languages that require using the system java command
+	 * to pass in file locations for cleaning.
+	 * 
+	 * @param args If provided, the first element of this array must be the
+	 * absolute path of the json file.  If not provided, the code will look
+	 * in the "current location" for data.txt.  The output is written to the
+	 * same location as the input, with ".out" appended.
+	 */
 	public static void main(String[] args) {
 		// Using the default location of "here" if no file is specified.
 		File phil = new File("");
 		String inputLocation = phil.getAbsolutePath() + "/data.txt";
 		String outputLocation = phil.getAbsolutePath() + "/data.txt.out";
-		
+
+		// Over-write with the first input parameter, if provided.
+		if (args.length > 0) {
+			inputLocation = args[0];
+			outputLocation = args[0] + ".out";
+		}
+
 		System.out.println("Attempting to read file at " + inputLocation);
-		
-		try(FileReader reader = new FileReader(inputLocation);
+
+		// Load data, remove duplicates, and write results
+		try (FileReader reader = new FileReader(inputLocation); 
 				FileWriter writer = new FileWriter(outputLocation);) {
-			
+
 			JsonCleaner cleaner = new JsonCleaner(reader);
 			cleaner.cleanDuplicates();
 			cleaner.write(writer);
-			
+
 		} catch (FileNotFoundException e) {
-			System.out.println("The file could not be found.  Please check "
+			System.out.println("The file could not be found.  Please check " 
 					+ "the location and run again."
 					+ "\nNote that some systems are case sensitive so "
 					+ "\"data.txt\" is different than \"DATA.txt\".");
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 }
--- HSoftwareChallenge/src/JsonCleanerTest.java
+++ HSoftwareChallenge/src/JsonCleanerTest.java
@@ -3,10 +3,6 @@ import static org.junit.Assert.*;
 import java.io.StringReader;
 import java.io.StringWriter;
 
-import javax.json.Json;
-import javax.json.stream.JsonGenerator;
-import javax.json.stream.JsonParser;
-
 import org.junit.Test;
 
 public class JsonCleanerTest {
@@ -25,14 +21,85 @@ public class JsonCleanerTest {
 	}
 	
 	@Test
-	public void testDirectCopyEmpty() {
+	public void testWriteWithoutDuplicateRemoval() {
+		String inputJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\": {"
+									+ "\"hello\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "}"
+						+ "}";
+		String expectedJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":{"
+									+ "\"hello\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "}"
+							+ "}";
 		try{ 
 			StringWriter sw = new StringWriter();
-			JsonCleaner thisCleaner = new JsonCleaner(new StringReader("{}"));
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
 			thisCleaner.write(sw);
-			System.out.println("Generator String:" + sw.toString());
-			assertEquals("Cleaner should pass back basic copied document", 
-					"{}", sw.toString());
+			assertEquals("Cleaner does not copy until removeDuplicates is called", 
+					expectedJson, sw.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("No exceptions should occur during construction");
+		}		
+	}
+
+	@Test
+	public void testCleanNoDups() {
+		String inputJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\": {"
+									+ "\"goodnight\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "}"
+						+ "}";
+		String expectedJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":{"
+								+ "\"goodnight\":\"moon\","
+								+ "\"this\":\"stays\""
+								+ "}"
+							+ "}";
+		try{ 
+			StringWriter sw = new StringWriter();
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
+			thisCleaner.cleanDuplicates();
+			thisCleaner.write(sw);
+			assertEquals("Problem during cleaning process: non duplicate items were removed", 
+					expectedJson, sw.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("No exceptions should occur during construction");
+		}		
+	}
+
+	
+	@Test
+	public void testCleanOneDup() {
+		String inputJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\": {"
+									+ "\"hello\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "}"
+						+ "}";
+		String expectedJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":{"
+									+ "\"this\":\"stays\""
+								+ "}"
+							+ "}";
+		try{ 
+			StringWriter sw = new StringWriter();
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
+			thisCleaner.cleanDuplicates();
+			thisCleaner.write(sw);
+			assertEquals("Problem during cleaning process", 
+					expectedJson, sw.toString());
 		} catch(Exception e) {
 			e.printStackTrace();
 			fail("No exceptions should occur during construction");
@@ -40,14 +107,156 @@ public class JsonCleanerTest {
 	}
 	
 	@Test
-	public void testDirectCopyOneKeyPair() {
+	public void testCleanWithArrays() {
+		String inputJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":[{"
+									+ "\"hello\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "}]"
+						+ "}";
+		String expectedJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":[{"
+									+ "\"this\":\"stays\""
+								+ "}]"
+							+ "}";
 		try{ 
 			StringWriter sw = new StringWriter();
-			JsonCleaner thisCleaner = new JsonCleaner(new StringReader("{\"sarah\":\"hello\"}"));
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
+			thisCleaner.cleanDuplicates();
 			thisCleaner.write(sw);
-			System.out.println("Generator String:" + sw.toString());
-			assertEquals("Cleaner should pass back basic copied document", 
-					"{\"sarah\":\"hello\"}", sw.toString());
+			assertEquals("Problem during cleaning process", 
+					expectedJson, sw.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("No exceptions should occur during construction");
+		}		
+	}
+	
+	@Test
+	public void testCleanStartAsArray() {
+		String inputJson = "[{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":[{"
+									+ "\"hello\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "}]"
+							+ "}]";
+		String expectedJson = "[{"
+									+ "\"hello\":\"world\","
+									+ "\"goodbye\":[{"
+										+ "\"this\":\"stays\""
+									+ "}]"
+								+ "}]";
+		try{ 
+			StringWriter sw = new StringWriter();
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
+			thisCleaner.cleanDuplicates();
+			thisCleaner.write(sw);
+			assertEquals("Problem during cleaning process", 
+					expectedJson, sw.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("No exceptions should occur during construction");
+		}		
+	}
+	
+	@Test
+	public void testCleanSiblings() {
+		String inputJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\": {"
+									+ "\"hello\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "},"
+								+ "\"sibling\": {"
+									+ "\"hello\":\"moon\","
+									+ "\"this\":\"stays\""
+								+ "}"
+						+ "}";
+		String expectedJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":{"
+									+ "\"this\":\"stays\""
+								+ "},"
+								+ "\"sibling\":{"
+									+ "\"this\":\"stays\""
+								+ "}"
+							+ "}";
+		try{ 
+			StringWriter sw = new StringWriter();
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
+			thisCleaner.cleanDuplicates();
+			thisCleaner.write(sw);
+			assertEquals("Problem during cleaning process", 
+					expectedJson, sw.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("No exceptions should occur during construction");
+		}		
+	}
+	
+	@Test
+	public void testCleanComplex() {
+		String inputJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":{"
+									+ "\"this\":\"hello\","
+									+ "\"that\":[\"moon\","
+												+ "{\"hello\":\"this\"},"
+												+ "\"hello\""
+												+ "]"
+								+ "}"
+						+ "}";
+		String expectedJson = "{"
+								+ "\"hello\":\"world\","
+								+ "\"goodbye\":{"
+									+ "\"this\":\"hello\","
+									+ "\"that\":[\"moon\","
+												+ "{},"
+												+ "\"hello\""
+												+ "]"
+								+ "}"
+						+ "}";
+		try{ 
+			StringWriter sw = new StringWriter();
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
+			thisCleaner.cleanDuplicates();
+			thisCleaner.write(sw);
+			assertEquals("Problem during cleaning process", 
+					expectedJson, sw.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("No exceptions should occur during construction");
+		}		
+	}
+	
+	@Test
+	public void testCleanExampleGiven() {
+		String inputJson = "{"
+							+ "\"wholesaler\":\"US Foods\","
+							+ "\"delivered\":\"2015-06-19T05:15:00-0500\"," 
+							+ "\"contacts\":["
+									+ "{\"wholesaler\":\"US Foods\",\"name\":\"John Lederer\"}," 
+									+ "{\"wholesaler\":\"Sysco\",\"name\":\"Bill Delaney\"}"
+								+ "]"
+							+ "}";
+		String expectedJson = "{"
+								+ "\"wholesaler\":\"US Foods\","
+								+ "\"delivered\":\"2015-06-19T05:15:00-0500\"," 
+								+ "\"contacts\":["
+										+ "{\"name\":\"John Lederer\"}," 
+										+ "{\"name\":\"Bill Delaney\"}"
+									+ "]"
+								+ "}";
+		try{ 
+			StringWriter sw = new StringWriter();
+			JsonCleaner thisCleaner = new JsonCleaner(new StringReader(inputJson));
+			thisCleaner.cleanDuplicates();
+			thisCleaner.write(sw);
+			assertEquals("Problem during cleaning process", 
+					expectedJson, sw.toString());
 		} catch(Exception e) {
 			e.printStackTrace();
 			fail("No exceptions should occur during construction");
